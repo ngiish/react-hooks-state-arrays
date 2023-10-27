67c8aede4500f31e363d40be2c918436de5a6868
Isaac <govegeku00@gmail.com>
2023-10-27 21:57:23 +0300

    New Commit

--- src/components/SpicyFoodList.js
+++ src/components/SpicyFoodList.js
@@ -23,10 +23,34 @@ function SpicyFoodList() {
       }
     })
     setFoods(newFoodArray)
-
   }
 
-  const foodList = foods.map((food) => (
+  function handleFilterChange(event){
+    setFilterBy(event.target.value)
+  }
+
+  return (
+    <select name="filter" onChange={handleFilterChange}>
+      <option value="All">All</option>
+      <option value="American">American</option>
+      <option value="Sichuan">Sichuan</option>
+      <option value="Thai">Thai</option>
+      <option value="Mexican">Mexican</option>
+    </select>
+  )
+
+  const [foods, setFoods] = useState(spicyFoods)
+  const [filterBy, setFilterBy] = useState("All")
+
+  const foodsToDisplay = foods.filter((food) => {
+    if (filterBy === "All") {
+      return true
+    } else {
+      return food.cuisine === filterBy
+    }
+  })
+
+  const foodList = foodsToDisplay.map((food) => (
     <li key={food.id} onClick={() => handleClick(food.id)}>
       {food.name} | Heat: {food.heatLevel} | Cuisine: {food.cuisine}
     </li>
