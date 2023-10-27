bc710e3319c2acb874ca0dca8c6d4ea9ecc10c6c
Isaac <govegeku00@gmail.com>
2023-10-27 21:14:18 +0300

    Next Commit

--- src/components/SpicyFoodList.js
+++ src/components/SpicyFoodList.js
@@ -11,9 +11,14 @@ function SpicyFoodList() {
 
     console.log(newFood);
   }
+  function handleClick(id){
+    const newFoodArray = foods.filter((food) => food.id != id)
+    setFoods(newFoodArray)
+
+  }
 
   const foodList = foods.map((food) => (
-    <li key={food.id}>
+    <li key={food.id} onClick={() => handleClick(food.id)}>
       {food.name} | Heat: {food.heatLevel} | Cuisine: {food.cuisine}
     </li>
   ));
